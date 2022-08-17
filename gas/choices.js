0e0a8f55fa47b3baf9448b8a1f9a17daabd20fa7
Yuya Tanaka <yuya.presto@gmail.com>
2022-08-17 16:01:06 +0900

    Include columns which do not have others option in choices.csv

--- gas/choices.js
+++ gas/choices.js
@@ -20,38 +20,45 @@ function getChoices() {
     const titleAndChoicesMap = form.getItems()
         .filter(item => itemAcceptsResponseSet.has(item.getType()))
         .map(item => getTitleAndChoicesWithOther(item));
-    titleAndChoicesMap.forEach(([title, choices]) => {
+    titleAndChoicesMap.forEach(({ title, choices }) => {
         if (choices === null || choices === void 0 ? void 0 : choices.some(c => c.includes(','))) {
             throw new Error(`Item "${title}" has a comma in one of its choices.`);
         }
     });
-    const spreadSheet = SpreadsheetApp.create(`${form.getTitle()}-choices-with-other`, titleAndChoicesMap.length + 1, 2);
+    const spreadSheet = SpreadsheetApp.create(`${form.getTitle()}-choices`, titleAndChoicesMap.length + 1, 4);
     const sheet = spreadSheet.getSheets()[0];
-    sheet.getRange(1, 1, 1, 2).setValues([['設問文章', '選択肢']]);
-    titleAndChoicesMap.forEach(([title, choices], i) => {
+    sheet.getRange(1, 1, 1, 4).setValues([['設問文章', '選択肢', '複数回答', '自由記述']]);
+    titleAndChoicesMap.forEach(({ title, choices, hasMultiple, hasOther }, i) => {
         var _a;
-        sheet.getRange(i + 2, 1, 1, 2).setValues([[title, (_a = choices === null || choices === void 0 ? void 0 : choices.join(',')) !== null && _a !== void 0 ? _a : '']]);
+        sheet.getRange(i + 2, 1, 1, 4).setValues([[title, (_a = choices === null || choices === void 0 ? void 0 : choices.join(',')) !== null && _a !== void 0 ? _a : '', hasMultiple ? 1 : 0, hasOther ? 1 : 0]]);
     });
     sheet.setColumnWidth(2, 1200);
     Logger.log('SpreadSheet created on %s', spreadSheet.getUrl());
 }
 function getTitleAndChoicesWithOther(item) {
     let choices = null;
+    let hasMultiple = false;
+    let hasOther = false;
     switch (item.getType()) {
+        case FormApp.ItemType.LIST:
+            {
+                const concreteItem = item.asListItem();
+                choices = concreteItem.getChoices().map(c => c.getValue());
+            }
+            break;
         case FormApp.ItemType.MULTIPLE_CHOICE:
             { // Radio Buttons
                 const concreteItem = item.asMultipleChoiceItem();
-                if (concreteItem.hasOtherOption()) {
-                    choices = concreteItem.getChoices().map(c => c.getValue());
-                }
+                choices = concreteItem.getChoices().map(c => c.getValue());
+                hasOther = concreteItem.hasOtherOption();
             }
             break;
         case FormApp.ItemType.CHECKBOX: {
             const concreteItem = item.asCheckboxItem();
-            if (concreteItem.hasOtherOption()) {
-                choices = concreteItem.getChoices().map(c => c.getValue());
-            }
+            choices = concreteItem.getChoices().map(c => c.getValue());
+            hasMultiple = true;
+            hasOther = concreteItem.hasOtherOption();
         }
     }
-    return [item.getTitle(), choices];
+    return { title: item.getTitle(), choices, hasMultiple, hasOther };
 }
