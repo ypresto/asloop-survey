32a454e272861d999d1b8bdc136b9928429214ed
Yuya Tanaka <yuya.presto@gmail.com>
2023-10-24 21:50:04 +0900

    Extract plotting code from plot.ipynb

---
+++ plot.py
@@ -0,0 +1,150 @@
+# Plotting
+
+import pandas as pd
+from pandas.testing import assert_series_equal
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.ticker as ticker
+from seaborn import FacetGrid
+from matplotlib.figure import Figure
+from matplotlib.container import BarContainer
+from matplotlib.patheffects import withStroke
+
+palette_6 = sns.color_palette("Blues", 6)
+palette_6.reverse()
+palette_4 = sns.color_palette("Blues", 4)
+palette_4.reverse()
+palette_3 = sns.color_palette("Blues", 3)
+palette_3.reverse()
+
+sns.set_style('whitegrid')
+sns.set_palette(palette_6, 6)
+mpl.rcParams['figure.dpi'] = 144
+mpl.rcParams['font.family'] = 'Hiragino Sans'
+mpl.rcParams["patch.force_edgecolor"] = False
+
+percent_locator = ticker.MaxNLocator(10, steps=[1, 2, 2.5, 5, 10])
+
+
+def adjust_figure_for_v(figure: Figure, n: int | None, title: str, description: str = '', percent: bool = True, bar_label: bool = True):
+    ax = figure.axes[0]
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_color('black')
+    ax.xaxis.grid(False)
+    ax.margins(x=0.05)
+    if percent:
+        ax.yaxis.set_major_formatter(ticker.PercentFormatter())
+        ax.yaxis.set_major_locator(percent_locator)
+        if bar_label:
+            for container in ax.containers:
+                ax.bar_label(container, fmt='%.1f%%', padding=4)
+        # 101 for prevent tick label from being clipped by frame.
+        ax.set_ylim(ymin=0, ymax=101 if ax.get_ylim()
+                    [1] >= 100 else None, auto=None)
+    # figure.suptitle(title)
+    figure.subplots_adjust(top=0.9, right=0.9, left=0.1)
+    if n is not None:
+        figure.text(0.98, 0.12, f"n={n}", ha='right')
+    if description:
+        figure.text(0.03, -0.02, description, ha='left', va='top')
+
+
+def adjust_figure_for_h(figure: Figure, n: int | None, title: str, description: str = '', percent: bool = True, bar_label: bool = True):
+    ax = figure.axes[0]
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_color('black')
+    ax.yaxis.grid(False)
+    ax.margins(y=0.05)
+    if percent:
+        ax.xaxis.set_major_formatter(ticker.PercentFormatter())
+        ax.xaxis.set_major_locator(percent_locator)
+        if bar_label:
+            for container in ax.containers:
+                ax.bar_label(container, fmt='%.1f%%', padding=4)
+        # 101 for prevent tick label from being clipped by frame.
+        ax.set_xlim(xmin=0, xmax=101 if ax.get_xlim()
+                    [1] >= 100 else None, auto=None)
+    # figure.suptitle(title)
+    figure.subplots_adjust(top=0.9, right=0.9)
+    if n is not None:
+        figure.text(0.98, 0.12, f"n={n}", ha='right')
+    if description:
+        figure.text(0.03, -0.02, description, ha='left', va='top')
+
+
+def adjust_figure_for_h_stack(figure: Figure, n: int | None, title: str, description: str = '', palette=None):
+    adjust_figure_for_h(figure, n, title, description,
+                        percent=True, bar_label=False)
+
+    class ReversePercentFormatter(ticker.PercentFormatter):
+        def format_pct(self, x, display_range) -> str:
+            return super().format_pct(100 - x, display_range)
+    ax = figure.axes[0]
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.invert_xaxis()
+    # -1 for prevent tick label from being clipped by frame.
+    ax.set_xlim(xmin=100, xmax=-3, auto=None)
+    ax.xaxis.set_major_formatter(ReversePercentFormatter())
+    container: BarContainer
+
+    for i, container in enumerate(reversed(ax.containers)):
+        if i >= 2:
+            color = '#000'
+        else:
+            color = '#fff'
+        path_effects = None
+        if len(ax.containers) >= 2:
+            path_effects = [withStroke(
+                linewidth=2.5, foreground=palette[i] if palette else f'C{i}')]
+        if i == len(ax.containers) - 1:
+            for patch in container.patches:
+                patch.set_edgecolor('none')
+        ax.bar_label(container, fmt='%.1f%%', padding=4, label_type='center',
+                     fontsize=8, fontweight='bold', color=color, path_effects=path_effects)
+    figure.subplots_adjust(top=0.88)
+    if figure.legends:
+        sns.move_legend(figure, 'upper center',
+                        bbox_to_anchor=(0.5, 0.95), ncol=6, title='')
+
+
+def _adjust_figure_for_grouped(figure: Figure):
+    ax = figure.axes[0]
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    figure.subplots_adjust(top=0.88)
+    if figure.legends:
+        sns.move_legend(figure, 'upper center',
+                        bbox_to_anchor=(0.5, 0.95), ncol=6, title='')
+
+
+def adjust_figure_for_v_grouped(figure: Figure, n: int | None, title: str, description: str = '', percent: bool = True, bar_label: bool = True):
+    adjust_figure_for_v(figure, n, title, description,
+                        percent=percent, bar_label=bar_label)
+    _adjust_figure_for_grouped(figure)
+
+
+def adjust_figure_for_h_grouped(figure: Figure, n: int | None, title: str, description: str = '', percent: bool = True, bar_label: bool = True):
+    adjust_figure_for_h(figure, n, title, description,
+                        percent=percent, bar_label=bar_label)
+    _adjust_figure_for_grouped(figure)
+
+
+def vbar(data_series: pd.Series, n: int | None, title: str, description: str = '', percent: bool = True):
+    grid: FacetGrid = sns.catplot(data=data_series.to_frame().transpose(
+    ), kind='bar', orient='v', width=0.5, height=5, aspect=16/9, color="C0")
+    figure: Figure = grid.figure
+    adjust_figure_for_v(figure, n, title, description, percent)
+    return figure
+
+
+def hbar(data_series: pd.Series, n: int | None, title: str, description: str = '', percent: bool = True):
+    grid: FacetGrid = sns.catplot(data=data_series.to_frame().transpose(
+    ), kind='bar', orient='h', width=0.5, height=5, aspect=16/9, color="C0")
+    figure: Figure = grid.figure
+    adjust_figure_for_h(figure, n, title, description, percent)
+    return figure
